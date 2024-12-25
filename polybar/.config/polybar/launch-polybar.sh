206af8eae326f11f892ad5a6e6a25b90373fb900
hafezfhmi <hafezfahmi2000@gmail.com>
2024-12-26 06:30:10 +0800

    fix: fix polybar launch script

--- polybar/.config/polybar/launch-polybar.sh
+++ polybar/.config/polybar/launch-polybar.sh
@@ -6,8 +6,6 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 | tee -a /tmp/polybar1.log & disown
-
-echo "Bars launched..."
+# Launch bar1
+echo "---" | tee -a /tmp/polybar1.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
