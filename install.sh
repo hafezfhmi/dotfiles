3799a1f46330ae71cf01fec28d3e6f0c459b08a3
hafezfhmi <hafezfahmi2000@gmail.com>
2025-10-13 01:57:35 +0800

    feat: rename nvim installation folder

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ echo "--- Installing neovim ---"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo mv /opt/nvim-linux-x86_64 /opt/nvim
 rm nvim-linux-x86_64.tar.gz
 
 echo "--- Installing Nerd Fonts JetbrainsMono ---"
