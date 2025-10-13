ebea80cfb060b5e783ef56267264d303b7f11b81
hafezfhmi <hafezfahmi2000@gmail.com>
2025-10-13 19:13:22 +0800

    feat: setup node.js installation script

--- install.sh
+++ install.sh
@@ -48,3 +48,21 @@ mkdir -p ~/.local/bin
 ln -sf $(which fdfind) ~/.local/bin/fd
 
 echo "--- Completed installing lazyvim requirements ---"
+
+echo "--- Installing Node.js ---"
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.20.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.3".
+
+source ~/.bashrc
